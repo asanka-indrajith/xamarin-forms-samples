dc9823769b01ce0699882ccc54aaa30fa80a11b8
asanka-indrajith <ddasankaindrajith@gmail.com>
2015-04-24 17:47:44 +0530

    Changed app.cs
    aaassaaa

--- BluetoothHeartRateMonitor/HeartRateMonitor.Core/App.cs
+++ BluetoothHeartRateMonitor/HeartRateMonitor.Core/App.cs
@@ -12,7 +12,7 @@ namespace HeartRateMonitor
 		{	
 			return new NavigationPage (new DeviceList (Adapter));
 		}
-        //setting adapter
+        //setting adapter 222
 		public static void SetAdapter (IAdapter adapter) {
 			Adapter = adapter;
 		}
